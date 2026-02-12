72026c8e6bf7189ae7b0a096b8dc61b0571b8087
maxsudbekjon <toshpulatovm73@gmail.com>
2026-02-12 15:27:48 +0500

    add statis

--- api_project/settings.py
+++ api_project/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
